62c042a03382c741502b93092518ca1f8e17f788
Amr Fahmy <amrfahmy57@gmail.com>
2023-12-22 21:24:23 +0200

    add vercel analytics

--- packages/blog-starter-kit/themes/personal/components/layout.tsx
+++ packages/blog-starter-kit/themes/personal/components/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from './analytics';
 import { Integrations } from './integrations';
 import { Meta } from './meta';
 import { Scripts } from './scripts';
+import { Analytics as An } from '@vercel/analytics/react';
 
 type Props = {
 	children: React.ReactNode;
@@ -14,6 +15,7 @@ export const Layout = ({ children }: Props) => {
 			<Scripts />
 			<div className="min-h-screen bg-white dark:bg-neutral-950">
 				<main>{children}</main>
+				<An />
 			</div>
 			<Analytics />
 			<Integrations />
